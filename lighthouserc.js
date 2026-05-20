df1030b8a799228596f7ca7e5f422aaa626c5bdc
Manon Valdonio <manonvaldonio@gmail.com>
2026-05-20 10:23:18 +0200

    cicd: add lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:5173/'],
+      numberOfRuns: 3,
+      settings: {
+        chromeFlags: '--no-sandbox',
+        preset: 'desktop',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:accessibility': ['warn', { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
