f274859d7e1e7b7f6d7c140e79ee8b7979e0687b
Manon Valdonio <manonvaldonio@gmail.com>
2026-06-12 14:10:48 +0200

    fix: ci cd

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,30 +15,10 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        'categories:performance': [
-          'error',
-          { minScore: 0.7 },
-          'warn',
-          { minScore: 0.9 },
-        ],
-        'categories:accessibility': [
-          'error',
-          { minScore: 0.7 },
-          'warn',
-          { minScore: 0.9 },
-        ],
-        'categories:best-practices': [
-          'error',
-          { minScore: 0.7 },
-          'warn',
-          { minScore: 0.9 },
-        ],
-        'categories:seo': [
-          'error',
-          { minScore: 0.7 },
-          'warn',
-          { minScore: 0.9 },
-        ],
+        'categories:performance': ['error', { minScore: 0.7 }],
+        'categories:accessibility': ['error', { minScore: 0.7 }],
+        'categories:best-practices': ['error', { minScore: 0.7 }],
+        'categories:seo': ['error', { minScore: 0.7 }],
       },
     },
     upload: {
